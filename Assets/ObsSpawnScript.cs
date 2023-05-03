9413178489b9e66049fdbfcae41e33bce2868edf
makulugia <67037573+makulugia@users.noreply.github.com>
2023-05-03 09:07:34 -0500

    Add files via upload

---
+++ Assets/ObsSpawnScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsSpawnScript : MonoBehaviour
+{
+    public GameObject obs;
+    public float spawnRate = 6;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnObs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnObs();
+            timer = 0;
+        }
+    }
+
+    void spawnObs()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(obs, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+    }
+}
